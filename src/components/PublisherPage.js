d9309b4f3c6f8c3bff0b0134560125b1c4a3a630
OstlerDev <OstlerDev@users.noreply.github.com>
2017-10-06 15:59:45 -0700

    Change around sizing

--- src/components/PublisherPage.js
+++ src/components/PublisherPage.js
@@ -12,10 +12,10 @@ class PublisherPage extends Component {
 					{/* Profile Picture */}					
 				</div>
 				{/* Artifacts content box */}
-				<div className="container-fluid">
+				<div className="container">
 					{/* Profile Info */}
 					<div className="row" style={{height: "100px", overflowY: "hidden"}}>	
-						<div className="col-12 col-sm-12 col-md-6 col-lg-5 col-xl-4" style={{height: "100px", padding: "20px"}}>
+						<div className="col-12 col-sm-12 col-md-7 col-lg-5 col-xl-5" style={{height: "100px", padding: "20px"}}>
 							<img style={{height: "100%"}} className="rounded-circle" src="https://static-cdn.jtvnw.net/jtv_user_pictures/bobross-profile_image-0b9dd167a9bb16b5-300x300.jpeg" />
 							<h5 style={{display: "inline", marginLeft: "20px", verticalAlign: "middle"}}>Bob Ross</h5>
 							<div className="btn-group" style={{display: "inline-block", marginTop: "10px", marginBottom: "10px"}}>
@@ -23,7 +23,7 @@ class PublisherPage extends Component {
 								<button className="btn btn-sm btn-outline-secondary" disabled>10 Followers</button>
 							</div>
 						</div>
-						<div className="col-12 col-sm-12 col-md-6 col-lg-7 col-xl-8" style={{padding: "20px"}}>
+						<div className="col-12 col-sm-12 col-md-5 col-lg-7 col-xl-7" style={{padding: "20px"}}>
 							Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum
 						</div>
 					</div>
