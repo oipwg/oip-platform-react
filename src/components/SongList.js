10139999bf40850ba09ad8d784e6aa3401d11f5e
OstlerDev <OstlerDev@users.noreply.github.com>
2017-10-24 11:28:23 -0700

    Adjust background color of active element

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -41,20 +41,20 @@ class SongList extends Component {
 					</div> 
 				</li>
 				{this.props.SongList.map(function(song, i){
-					return <li key={i} className={song.fname === _this.props.CurrentFile.fname ? "list-group-item list-group-item-secondary" : "list-group-item"} style={{padding: "0px", backgroundColor: _this.props.bgColor, border: "1px solid " + _this.props.mainColor}}>
+					return <li key={i} className="list-group-item" style={song.fname === _this.props.CurrentFile.fname ? {padding: "0px", backgroundColor: _this.props.mainColor, border: "1px solid " + _this.props.mainColor} : {padding: "0px", backgroundColor: _this.props.bgColor, border: "1px solid " + _this.props.mainColor}}>
 						<div style={{padding: "4px 5px", display:"flex"}}>
 							<img className="rounded" src={song.albumArtwork} width="40px" height="40px" alt="" />
 							<div style={{padding: "0px 10px", width:"235px"}}>
-								<div style={{color: _this.props.mainColor, fontWeight:"700",fontSize:"14px", textOverflow: "ellipsis", overflow: "hidden", whiteSpace: "nowrap"}}>{song.artist}</div>
-								<div style={{color: _this.props.mainColor, fontSize:"12px", width: "235px", display: "flex"}}>
+								<div style={song.fname === _this.props.CurrentFile.fname ? {color: _this.props.bgColor, fontWeight:"700",fontSize:"14px", textOverflow: "ellipsis", overflow: "hidden", whiteSpace: "nowrap"} : {color: _this.props.mainColor, fontWeight:"700",fontSize:"14px", textOverflow: "ellipsis", overflow: "hidden", whiteSpace: "nowrap"}}>{song.artist}</div>
+								<div style={song.fname === _this.props.CurrentFile.fname ? {color: _this.props.bgColor, fontSize:"12px", width: "235px", display: "flex"} : {color: _this.props.mainColor, fontSize:"12px", width: "235px", display: "flex"}}>
 									<div style={{width: "200px", textOverflow: "ellipsis", overflow: "hidden", whiteSpace: "nowrap"}}>{i + 1}: {song.name}</div>
 									<div style={{width: "30px", textAlign: "right"}}>{song.length ? song.length : ""}</div>
 								</div>
 							</div>
 							<div style={{margin: "auto", display: "flex"}}>
-								{song.disPlay ? "" : <button onClick={function(){_this.playSong(song)}} className={song.sugPlay ? "btn btn-sm btn-outline-success" : "btn btn-sm btn-outline-info"}><span className="icon icon-controller-play"></span> {song.sugPlay ? "$" + song.sugPlay : "Free"}</button>}
+								{song.disPlay ? "" : <button onClick={function(){_this.playSong(song)}} className={song.sugPlay ? "btn btn-sm btn-outline-success" : "btn btn-sm btn-outline-info"} style={song.fname === _this.props.CurrentFile.fname ? {backgroundColor: _this.props.bgColor} : {}}><span className="icon icon-controller-play"></span> {song.sugPlay ? "$" + song.sugPlay : "Free"}</button>}
 								<span style={{paddingLeft: "5px"}}></span>
-								{song.disBuy ? "" : <button className={song.sugBuy ? "btn btn-sm btn-outline-success" : "btn btn-sm btn-outline-info"}><span className="icon icon-download"></span> {song.sugBuy ? "$" + song.sugBuy : "Free"}</button>}
+								{song.disBuy ? "" : <button className={song.sugBuy ? "btn btn-sm btn-outline-success" : "btn btn-sm btn-outline-info"} style={song.fname === _this.props.CurrentFile.fname ? {backgroundColor: _this.props.bgColor} : {}}><span className="icon icon-download"></span> {song.sugBuy ? "$" + song.sugBuy : "Free"}</button>}
 							</div>
 						</div>
 					</li>
