bcb0542011e2b589e537587f4e6219c2ebadb3df
Ryan <ryan.chacon@alexandria.io>
2018-05-09 12:47:18 -0700

    converted ContentCardsContainer to dumb component

--- src/components/ContentCardsContainer.js
+++ src/components/ContentCardsContainer.js
@@ -2,27 +2,23 @@ import React, { Component } from 'react';
 
 import ContentCard from './contentCard.js'
 
-class ContentCardsContainer extends Component {
-	render() {
-		let _this = this;
-
-		return (
-			<div className="content-cards-container container">
-				<h4 >{this.props.title}</h4>
-				{this.props.opts.isFetching ? <p>Loading...</p> : ""}
-				{this.props.opts.error ? <p>Oops! Looks like something went wrong...</p> : ""}
-				<div className="row">
-					{(this.props.opts.items && this.props.opts.items.length > 0) ? this.props.opts.items.map(function(artifact, i){
-						return <ContentCard 
-								key = {i}
-								artifact = {artifact}
-								Core = {_this.props.Core}
-							/>
-					}) : ""}
-				</div>
-			</div>
-		);
-	}
-}
+const ContentCardsContainer = (props) => {
+	return (
+        <div className="content-cards-container container">
+            <h4 >{props.title}</h4>
+            {props.opts.isFetching ? <p>Loading...</p> : ""}
+            {props.opts.error ? <p>Oops! Looks like something went wrong...</p> : ""}
+            <div className="row">
+                {(props.opts.items && props.opts.items.length > 0) ? props.opts.items.map((artifact, i) => {
+                    return <ContentCard
+                        key = {i}
+                        artifact = {artifact}
+                        Core = {props.Core}
+                    />
+                }) : ""}
+            </div>
+        </div>
+	)
+};
 
 export default ContentCardsContainer;
