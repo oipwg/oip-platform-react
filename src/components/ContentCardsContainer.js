9f28d80ff60af797925328ba2290d08eb4558822
Ryan <ryan.chacon@alexandria.io>
2018-05-09 13:14:25 -0700

    refactored ContentCardsContainer

--- src/components/ContentCardsContainer.js
+++ src/components/ContentCardsContainer.js
@@ -3,22 +3,23 @@ import React, { Component } from 'react';
 import ContentCard from './contentCard.js'
 
 const ContentCardsContainer = (props) => {
-	return (
-        <div className="content-cards-container container">
-            <h4 >{props.title}</h4>
-            {props.opts.isFetching ? <p>Loading...</p> : ""}
-            {props.opts.error ? <p>Oops! Looks like something went wrong...</p> : ""}
-            <div className="row">
-                {(props.opts.items && props.opts.items.length > 0) ? props.opts.items.map((artifact, i) => {
-                    return <ContentCard
-                        key = {i}
-                        artifact = {artifact}
-                        Core = {props.Core}
-                    />
-                }) : ""}
-            </div>
+    return <div className="content-cards-container container">
+        <h4 className="content-cards-container-title">{props.title}</h4>
+
+        {props.opts.isFetching ? <p>Loading...</p> : ""}
+        {props.opts.error ? <p>Oops! Looks like something went wrong...</p> : ""}
+        {props.opts.items.length === 0 ? <i>No results found!</i> : null}
+
+        <div className="row">
+            { props.opts.items.map((artifact, i) => {
+                return <ContentCard
+                    key={i}
+                    artifact={artifact}
+                    Core={props.Core}
+                />
+            })}
         </div>
-	)
+    </div>
 };
 
 export default ContentCardsContainer;
