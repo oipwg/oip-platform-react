e67a3a1f6166f0fa7ac4ba472d0346a5ffd8634f
Ryan <ryan.chacon@alexandria.io>
2018-05-10 09:23:47 -0700

    content card spacing

--- src/components/contentCard.js
+++ src/components/contentCard.js
@@ -11,7 +11,7 @@ const ContentCard = (props) => {
 
 	const scrollToTop = () => {
 		window.scrollTo(0, 0);
-	}
+	};
 
     let title = props.artifact.getTitle();
     let publisher = props.artifact.getMainAddress();
@@ -19,29 +19,39 @@ const ContentCard = (props) => {
     let duration = props.artifact.getDuration();
 
     return (
-        <Link to={"/" + txid.substring(0,6) } onClick={scrollToTop} className="" title={title} >
-            <div className="card">
-                <div>
-                    <button className="btn btn-outline-primary btn-white">
-                        <ArtifactIcon artifact={props.artifact} />
-                    </button>
-                </div>
-                <div className="card-img-top content-card-img">
-                    <img src={"http://thumbs.oip.fun/artifact/" + txid.substr(0,6)} alt="" />
-                </div>
-                {duration ? <p className="content-card-xinfo"><FormattedTime numSeconds={duration} /></p> : <div className="content-card-xinfo-offset"></div>}
-                <div className="card-block" >
-                    <strong>{title}</strong>
-                    <Link to={"/pub/" + publisher } onClick={function(){window.scrollTo(0, 0)}}>
-                        <div>
-                            <PublisherIcon id={publisher} Core={props.Core} small={true} />
-                            <p>{props.artifact.publisherName}</p>
+                <div className="card border-0 mb-5" style={{width: 210}}>
+                    <Link to={"/" + txid.substring(0,6) } onClick={scrollToTop} className="" title={title} >
+
+                        <button className="btn btn-outline-primary btn-white">
+                            <ArtifactIcon artifact={props.artifact} />
+                        </button>
+                        <div className="card-img-top content-card-img">
+                            <img style={{width: 210}} src={"http://thumbs.oip.fun/artifact/" + txid.substr(0,6)} alt="" />
                         </div>
+
+
+
+
+                        {duration ? <p className="content-card-xinfo"><FormattedTime numSeconds={duration} /></p> : <div className="content-card-xinfo-offset"></div>}
                     </Link>
-                </div>
+
+                    <div className="row">
+                        <div className="col"><strong>{title}</strong></div>
+                        <div className="col">
+                            <Link to={"/pub/" + publisher } onClick={scrollToTop}>
+                                <PublisherIcon id={publisher} Core={props.Core} small={true} />
+                                <span>{props.artifact.publisherName}</span>
+                            </Link>
+                        </div>
+                    </div>
+
+
+
+
+
             </div>
-        </Link>
+
     );
-}
+};
 
 export default ContentCard;
