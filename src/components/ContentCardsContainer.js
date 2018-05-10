4d599f5df3f45be39982a55c11b6f2f017812ec0
Ryan <ryan.chacon@alexandria.io>
2018-05-09 19:17:50 -0700

    card container margins

--- src/components/ContentCardsContainer.js
+++ src/components/ContentCardsContainer.js
@@ -3,19 +3,27 @@ import React, { Component } from 'react';
 import ContentCard from './contentCard.js'
 
 const ContentCardsContainer = (props) => {
-    return <div className="content-cards-container container">
-        <h4 className="content-cards-container-title">{props.title}</h4>
+    return <div className="content-cards-container">
+        <div className="card-container">
 
-        {props.opts.isFetching ? <p>Loading...</p> : ""}
-        {props.opts.error ? <p>Oops! Looks like something went wrong...</p> : ""}
-        <div className="row">
-            { props.opts.items.map((artifact, i) => {
-                return <ContentCard
-                    key={i}
-                    artifact={artifact}
-                    Core={props.Core}
-                />
-            })}
+            <div className="content-cards-title-container">
+                <span className="card-container-title">
+                    {props.title}
+                </span>
+            </div>
+
+            {props.opts.isFetching ? <p>Loading...</p> : ""}
+            {props.opts.error ? <p>Oops! Looks like something went wrong...</p> : ""}
+
+            <div className="row no-gutters d-flex justify-content-between">
+                { props.opts.items.map((artifact, i) => {
+                    return <ContentCard
+                        key={i}
+                        artifact={artifact}
+                        Core={props.Core}
+                    />
+                })}
+            </div>
         </div>
     </div>
 };
