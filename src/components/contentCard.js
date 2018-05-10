e6921c2dd9bcc4665dd7e164f3f2468eb01ad525
Ryan <ryan.chacon@alexandria.io>
2018-05-10 10:33:40 -0700

    added public folder back

--- src/components/contentCard.js
+++ src/components/contentCard.js
@@ -19,26 +19,26 @@ const ContentCard = (props) => {
     let duration = props.artifact.getDuration();
 
     return (
-                <div className="card border-0 mb-5" style={{width: 210}}>
-                    <Link to={"/" + txid.substring(0,6) } onClick={scrollToTop} className="" title={title} >
+            <div className="card border-0 mb-5" style={{width: 210}}>
+                <Link to={"/" + txid.substring(0,6) } onClick={scrollToTop} className="" title={title} >
 
-                        <img className="card-img" style={{width: 210}} src={"http://thumbs.oip.fun/artifact/" + txid.substr(0,6)} alt="" />
-                        <div className="card-img-overlay">
-                            <button className="btn btn-outline-light card-title"><ArtifactIcon artifact={props.artifact} /></button>
-                            {duration ? <p className="content-card-xinfo"><FormattedTime numSeconds={duration} /></p> : <div className="content-card-xinfo-offset"></div>}
-                    </Link>
+                    <img className="card-img" style={{width: 210}} src={"http://thumbs.oip.fun/artifact/" + txid.substr(0,6)} alt="" />
+                    <div className="card-img-overlay">
+                        <button className="btn btn-outline-light card-title"><ArtifactIcon artifact={props.artifact} /></button>
+                        {duration ? <p className="content-card-xinfo"><FormattedTime numSeconds={duration} /></p> : <div className="content-card-xinfo-offset"></div>}
+                    </div>
+                </Link>
 
-                    <div className="row">
-                        <div className="col"><strong>{title}</strong></div>
-                        <div className="col">
-                            <Link to={"/pub/" + publisher } onClick={scrollToTop}>
-                                <PublisherIcon id={publisher} Core={props.Core} small={true} />
-                                <span>{props.artifact.publisherName}</span>
-                            </Link>
-                        </div>
+                <div className="row">
+                    <div className="col"><strong>{title}</strong></div>
+                    <div className="col">
+                        <Link to={"/pub/" + publisher } onClick={scrollToTop}>
+                            <PublisherIcon id={publisher} Core={props.Core} small={true} />
+                            <span>{props.artifact.publisherName}</span>
+                        </Link>
                     </div>
                 </div>
-
+            </div>
     );
 };
 
