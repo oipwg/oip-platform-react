a16bb370b48d3ff4c60d2cf793af80cc30a5ad4a
Ryan <ryan.chacon@alexandria.io>
2018-05-11 12:02:15 -0700

    fixed dropdown popper css

--- src/components/UserNav.js
+++ src/components/UserNav.js
@@ -7,7 +7,7 @@ const UserNav = (props) => {
                     data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                 {props.pubName}
             </button>
-            <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+            <div style={{left: -107}} className="dropdown-menu" aria-labelledby="dropdownMenuButton">
                 <a className="dropdown-item" href="#">Action</a>
                 <a className="dropdown-item" href="#">Another action</a>
                 <a className="dropdown-item" href="#">Something else here</a>
