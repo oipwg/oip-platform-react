d5c388c65cb68a5cfed56bfd19626334a30dfc00
Ryan <ryan.chacon@alexandria.io>
2018-05-16 13:50:32 -0700

    cleanup

--- src/components/EditArtifactContainer.js
+++ src/components/EditArtifactContainer.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 
 class EditArtifactContainer extends Component {
-	render() {
-		return (
-			<div className="container">
-				<h1>Edit Artifact</h1>
-			</div>
-		);
-	}
+    render() {
+        return (
+            <div className="container">
+                <h1>Edit Artifact</h1>
+            </div>
+        );
+    }
 }
 
 export default EditArtifactContainer;
