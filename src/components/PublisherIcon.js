6f387e5e112b57794d4e5f377914f4681e91acc8
Ryan <ryan.chacon@alexandria.io>
2018-05-18 09:27:11 -0700

    optional props for stlying PublisherIcon

--- src/components/PublisherIcon.js
+++ src/components/PublisherIcon.js
@@ -46,7 +46,8 @@ class PublisherIcon extends Component {
 	render() {
 		return (
 			<div className="userImage" >
-				<img style={{width: this.props.width}} className="border-0" ref={image => this.image = image} src={this.state.avatarSrc} />
+				<img style={{width: this.props.width, maxWidth: this.props.maxWidth, height: this.props.height, maxHeight: this.props.maxHeight}}
+					 className="border-0" ref={image => this.image = image} src={this.state.avatarSrc} />
                 <span style={{fontSize: 13}} className="card-subtitle ml-2 text-muted">{this.props.pubName}</span>
 			</div>
 		);
