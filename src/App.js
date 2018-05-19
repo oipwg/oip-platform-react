05eecd403785e4655a62cceaa9675f7f0ce37ee2
Ryan <ryan.chacon@alexandria.io>
2018-05-18 17:56:09 -0700

    removed NotificationSystem as passed down prop

--- src/App.js
+++ src/App.js
@@ -114,11 +114,7 @@ class App extends Component {
                                 <Route path="/user/:page" render={props => ( this.props.User.isLoggedIn ? ( <UserPage {...props} /> ) : ( <Redirect to="/"/> ))} />
 								<Route path="/user/:page/:type" render={props => ( this.props.User.isLoggedIn ? ( <UserPage {...props} /> ) : ( <Redirect to="/"/> ))} />
 								<Route path="/user/:page/:type/:id" render={props => ( this.props.User.isLoggedIn ? ( <UserPage {...props} /> ) : ( <Redirect to="/"/> ))} />
-
-
-                                <Route path="/:id" render={props =>
-                                    <ContentPage {...props} store={this.props.store}  piwik={piwik} NotificationSystem={this.props.NotificationSystem} />}
-                                />
+                                <Route path="/:id" render={props => <ContentPage {...props} store={this.props.store}  piwik={piwik} />}/>
 
                                 {/* The switch will render the last Route if no others are found (aka 404 page.) */}
                                 <Route component={NoMatch} />
