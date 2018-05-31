f9158940e5e3e142c812b225c95928e74ec90a4a
Ryan <ryan.chacon@alexandria.io>
2018-05-31 08:19:53 -0700

    STLContainer stripped

--- src/components/StlContainer.js
+++ src/components/StlContainer.js
@@ -1,53 +1,14 @@
 import React, { Component } from 'react';
 
 class STLContainer extends Component {
-	constructor(props){
-		super(props);
-
-		this.state = {
-
-		}
-
-		this.stateDidUpdate = this.stateDidUpdate.bind(this);
-
-		let updateState = this.stateDidUpdate;
-
-		this.unsubscribe = this.props.store.subscribe(() => {
-			updateState();
-		});
-	}
-	stateDidUpdate(){
-		let newState = this.props.store.getState();
-
-		let currentArtifact, active, activeFile;
-
-		if (newState.CurrentArtifact)
-			currentArtifact = newState.CurrentArtifact;
-		if (newState.FilePlaylist){
-			active = newState.FilePlaylist.active;
-			activeFile = newState.FilePlaylist[active];
-		}
-
-		let stateObj = {
-			CurrentArtifact: currentArtifact,
-			ActiveFile: activeFile
-		}
-
-		if (stateObj && this.state !== stateObj){
-			this.setState(stateObj);
-		}
-	}
-	componentWillUnmount(){
-		this.unsubscribe();
-	}
-	componentDidMount(){
-		this.stateDidUpdate();
-	}
+	constructor(props) {
+        super(props);
+    }
 	render() {
 		let stlURL;
 
-		if (this.state.ActiveFile && this.state.CurrentArtifact && this.state.ActiveFile.info && this.state.CurrentArtifact.artifact){
-			stlURL = this.props.Core.util.buildIPFSURL(this.state.CurrentArtifact.artifact.getLocation(), this.state.ActiveFile.info.getFilename());
+		if (this.props.ActiveFile && this.props.Artifact && this.props.ActiveFile.info && this.props.artifact){
+			stlURL = this.props.buildIPFSURL(this.props.CurrentArtifact.artifact.getLocation(), this.state.ActiveFile.info.getFilename());
 		}
 
 		return (
