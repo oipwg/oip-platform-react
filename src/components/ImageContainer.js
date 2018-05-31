dd2773a71a6a9d4df7bfd9932c15ceacd8332b9f
Ryan <ryan.chacon@alexandria.io>
2018-05-31 08:28:19 -0700

    ImageContainer stripped

--- src/components/ImageContainer.js
+++ src/components/ImageContainer.js
@@ -4,56 +4,19 @@ import IPFSImage from './IPFSImage.js';
 class ImageContainer extends Component {
 	constructor(props) {
 		super(props);
-		this.state = {};
-
-		this.stateDidUpdate = this.stateDidUpdate.bind(this);
-
-		let updateState = this.stateDidUpdate;
-
-		this.unsubscribe = this.props.store.subscribe(() => {
-			updateState();
-		});
 	}
-	stateDidUpdate(){
-		let newState = this.props.store.getState();
 
-		let currentArtifact, active, activeFile;
-
-		if (newState.CurrentArtifact)
-			currentArtifact = newState.CurrentArtifact;
-		if (newState.FilePlaylist){
-			active = newState.FilePlaylist.active;
-			activeFile = newState.FilePlaylist[active];
-		}
-
-		let stateObj = {
-			CurrentArtifact: currentArtifact,
-			ActiveFile: activeFile
-		}
-
-		if (stateObj && this.state !== stateObj){
-			this.setState(stateObj, () => {
-
-			});
-		}
-	}
-	componentWillUnmount(){
-		this.unsubscribe();
-	}
-	componentDidMount(){
-		this.stateDidUpdate();
-	}
 	render() {
 		let hash = "";
 		let preview = false;
 
-		if (this.state.ActiveFile && ((this.state.ActiveFile.isPaid && !this.state.ActiveFile.hasPaid) && !this.state.ActiveFile.owned) && this.props.Core){
+		if (this.props.ActiveFile && ((this.props.ActiveFile.isPaid && !this.props.ActiveFile.hasPaid) && !this.props.ActiveFile.owned)){
 			preview = true;
 
-			hash = this.props.Core.util.buildIPFSShortURL(this.state.CurrentArtifact.artifact.getLocation(), this.state.CurrentArtifact.artifact.getThumbnail());
+			hash = this.props.buildIPFSShortURL(this.props.Artifact.getLocation(), this.props.Artifact.getThumbnail());
 		} else {
-			if (this.state.CurrentArtifact && this.state.ActiveFile){
-				hash = this.props.Core.util.buildIPFSShortURL(this.state.CurrentArtifact.artifact.getLocation(), this.state.ActiveFile.info);
+			if (this.props.Artifact && this.props.ActiveFile){
+				hash = this.props.buildIPFSShortURL(this.props.Artifact.getLocation(), this.props.ActiveFile.info);
 			}
 		}
 
