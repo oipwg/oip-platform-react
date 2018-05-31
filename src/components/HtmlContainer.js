89f3e03027741e2877d798e74648873c7139393c
Ryan <ryan.chacon@alexandria.io>
2018-05-31 08:23:30 -0700

    HTMLContainer stripped

--- src/components/HtmlContainer.js
+++ src/components/HtmlContainer.js
@@ -3,52 +3,14 @@ import React, { Component } from 'react';
 class HTMLContainer extends Component {
 	constructor(props){
 		super(props);
-
-		this.state = {
-
-		}
-
-		this.stateDidUpdate = this.stateDidUpdate.bind(this);
-
-		let updateState = this.stateDidUpdate;
-
-		this.unsubscribe = this.props.store.subscribe(() => {
-			updateState();
-		});
 	}
-	stateDidUpdate(){
-		let newState = this.props.store.getState();
 
-		let currentArtifact, active, activeFile;
-
-		if (newState.CurrentArtifact)
-			currentArtifact = newState.CurrentArtifact;
-		if (newState.FilePlaylist){
-			active = newState.FilePlaylist.active;
-			activeFile = newState.FilePlaylist[active];
-		}
-
-		let stateObj = {
-			CurrentArtifact: currentArtifact,
-			ActiveFile: activeFile
-		}
-
-		if (stateObj && this.state !== stateObj){
-			this.setState(stateObj);
-		}
-	}
-	componentWillUnmount(){
-		this.unsubscribe();
-	}
-	componentDidMount(){
-		this.stateDidUpdate();
-	}
 	render() {
 		let websiteURL;
 
-		if (this.state.ActiveFile && this.state.CurrentArtifact && this.state.ActiveFile.info && this.state.CurrentArtifact.artifact){
-			// websiteURL = this.state.CurrentArtifact.artifact.getFirstHTMLURL();
-			websiteURL = this.props.Core.util.buildIPFSURL(this.state.CurrentArtifact.artifact.getLocation(), this.state.ActiveFile.info.getFilename());
+		if (this.props.ActiveFile && this.props.Artifact && this.props.ActiveFile.info && this.props.artifact){
+			// websiteURL = this.props.Artifact.getFirstHTMLURL();
+			websiteURL = this.props.buildIPFSURL(this.props.Artifact.getLocation(), this.props.ActiveFile.info.getFilename());
 		}
 
 		return (
