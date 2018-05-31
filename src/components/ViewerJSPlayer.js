8e31021917da44cfb6d69625d743c4e128ec0446
Ryan <ryan.chacon@alexandria.io>
2018-05-31 08:25:27 -0700

    ViewerJSPlayer stripped

--- src/components/ViewerJSPlayer.js
+++ src/components/ViewerJSPlayer.js
@@ -3,51 +3,13 @@ import React, { Component } from 'react';
 class ViewerJSPlayer extends Component {
 	constructor(props){
 		super(props);
-
-		this.state = {
-
-		}
-
-		this.stateDidUpdate = this.stateDidUpdate.bind(this);
-
-		let updateState = this.stateDidUpdate;
-
-		this.unsubscribe = this.props.store.subscribe(() => {
-			updateState();
-		});
 	}
-	stateDidUpdate(){
-		let newState = this.props.store.getState();
 
-		let currentArtifact, active, activeFile;
-
-		if (newState.CurrentArtifact)
-			currentArtifact = newState.CurrentArtifact;
-		if (newState.FilePlaylist){
-			active = newState.FilePlaylist.active;
-			activeFile = newState.FilePlaylist[active];
-		}
-
-		let stateObj = {
-			CurrentArtifact: currentArtifact,
-			ActiveFile: activeFile
-		}
-
-		if (stateObj && this.state !== stateObj){
-			this.setState(stateObj);
-		}
-	}
-	componentWillUnmount(){
-		this.unsubscribe();
-	}
-	componentDidMount(){
-		this.stateDidUpdate();
-	}
 	render() {
 		let pdfURL;
 
-		if (this.state.ActiveFile && this.state.CurrentArtifact && this.state.ActiveFile.info && this.state.CurrentArtifact.artifact){
-			pdfURL = this.props.Core.util.buildIPFSURL(this.props.Core.util.buildIPFSShortURL(this.state.CurrentArtifact.artifact.getLocation(), this.state.ActiveFile.info));
+		if (this.props.ActiveFile && this.props.Artifact && this.props.ActiveFile.info && this.props.Artifact){
+			pdfURL = this.props.buildIPFSURL(this.props.Core.util.buildIPFSShortURL(this.state.Artifact.getLocation(), this.state.ActiveFile.info));
 		}
 		return (
 			<iframe title="pdf" style={{width:"100vw", height:"100%", overflow: "hidden"}} frameBorder="0" src={"http://viewerjs.org/ViewerJS/#" + pdfURL}></iframe>
