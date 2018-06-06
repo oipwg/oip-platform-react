a49202dabb52062fda7ffa5e18175d0ff35f9deb
Ryan <ryan.chacon@alexandria.io>
2018-06-06 16:49:05 -0700

    ImageContainer loading image

--- src/components/ImageContainer.js
+++ src/components/ImageContainer.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import IPFSImage from './IPFSImage.js';
 
 class ImageContainer extends Component {
 	constructor(props) {
@@ -9,21 +8,24 @@ class ImageContainer extends Component {
 	render() {
 		let hash = "";
 		let preview = false;
-        console.log(this.props.ActiveFile, this.props.Artifact)
+		console.log("HASH ONE ", hash)
 		if (this.props.ActiveFile && ((this.props.ActiveFile.isPaid && !this.props.ActiveFile.hasPaid) && !this.props.ActiveFile.owned)){
 			preview = true;
 			//.getThumbnail returns undefined because no subType: Image
-			hash = this.props.buildIPFSShortURL(this.props.Artifact.getLocation(), this.props.Artifact.getThumbnail());
+			hash = this.props.buildIPFSShortURL(this.props.Artifact.getLocation(), this.props.Artifact.getThumbnail().getFilename());
 		} else {
 			if (this.props.Artifact && this.props.ActiveFile){
 				hash = this.props.buildIPFSShortURL(this.props.Artifact.getLocation(), this.props.ActiveFile.info.getFilename());
 			}
 		}
 
+		let url = this.props.buildIPFSURL(hash);
+
 		return (
 			<div className="align-middle" style={{height: "100%", width: "100vw", verticalAlign: "middle"}}>
 				<div className="img-container" style={{height: "inherit", width: "auto", maxWidth: "100%", display: "block",margin: "auto"}}>
-					<IPFSImage hash={hash} cover={preview} width={preview ? "100%" : ""} />
+					{/*<IPFSImage hash={hash} cover={preview} width={preview ? "100%" : ""} />*/}
+                    <img src={url} alt={"You're image is supposed to be here!"}/>
 				</div>
 			</div>
 		);
