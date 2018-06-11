77752eb4c24c797700623dc4222c264c6ae5dcb8
Ryan <ryan.chacon@alexandria.io>
2018-06-11 11:47:06 -0700

    added redux-immutable package

--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@ import ReactDOM from 'react-dom';
 
 import { createStore, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
-import { createLogger } from 'redux-logger'
+import { createLogger } from 'redux-logger',
 
 import createHistory from 'history/createBrowserHistory'
 
-import { routerReducer, routerMiddleware } from 'react-router-redux'
+// import { routerReducer, routerMiddleware } from 'react-router-redux'
 import { combineReducers } from 'redux'
 import reducers from './reducers'
 
@@ -21,13 +21,15 @@ const logger = createLogger({
 
 const history = createHistory()
 
-let middleware = [ logger, thunk, routerMiddleware(history) ];
 
-reducers.router = routerReducer;
+// routerMiddleware(history) this goes in middleware
+
+let middleware = [ logger, thunk ];
+
+// reducers.router = routerReducer;
 
 let store = createStore(
   combineReducers(reducers),
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__({name: "Alexandria React"}),
   applyMiddleware(...middleware)
 )
 
