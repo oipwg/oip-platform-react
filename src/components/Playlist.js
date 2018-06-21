d46d27c4288bc23dd58c7b8bd9ae4564d4dd9314
Ryan <ryan.chacon@alexandria.io>
2018-06-21 11:28:30 -0700

    prop-types && props => camelCase

--- src/components/Playlist.js
+++ src/components/Playlist.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 import { FormattedTime } from 'react-player-controls';
-
 import PaymentButtons from './PaymentButtons.js';
 
 class Playlist extends Component {
@@ -16,11 +16,11 @@ class Playlist extends Component {
 	getAllFiles(){
 		let files = [];
 
-		if (this.props.Artifact && this.props.FilePlaylist){
-			for (var key in this.props.FilePlaylist) {
+		if (this.props.artifact && this.props.filePlaylist){
+			for (var key in this.props.filePlaylist) {
 				// This just makes sure we are not getting the "active" key from the FilePlaylist obj
 				if (key.split("|").length === 2){
-					let newObj = this.props.FilePlaylist[key];
+					let newObj = this.props.filePlaylist[key];
 					newObj.key = key.toString();
 					files.push(newObj);
 				}
@@ -33,7 +33,7 @@ class Playlist extends Component {
 		let myArtifactFiles = [];
 
 		for (var file in files) {
-			if (files[file].key.split("|")[0] === this.props.Artifact.getTXID()){
+			if (files[file].key.split("|")[0] === this.props.artifact.getTXID()){
 				myArtifactFiles.push(files[file]);
 			}
 		}
@@ -57,14 +57,14 @@ class Playlist extends Component {
 	}
 
 	handleListClick(artifact, file) {
-		(file.key !== this.props.ActiveFile.key) ? (this.props.setCurrentFile(artifact, file)) : (null)
+		(file.key !== this.props.activeFile.key) ? (this.props.setCurrentFile(artifact, file)) : (null)
 	}
 
 	render() {
 		let _this = this;
 
 		let DisplayFiles = [];
-		let Artist = "";
+		let artist = "";
 
 		if (this.props.currentArtifactOnly){
 			DisplayFiles = this.getCurrentArtifactFiles();
@@ -73,9 +73,9 @@ class Playlist extends Component {
 		}
 
 		DisplayFiles = this.filterFiles(DisplayFiles, this.props.filter);
-		if (this.props.Artifact){
+		if (this.props.artifact){
 			//in current version, some artifacts may not have 'details'
-			Artist = this.props.Artifact.getDetail("artist")
+			artist = this.props.artifact.getDetail("artist")
 		}
 
 		return (
@@ -87,13 +87,14 @@ class Playlist extends Component {
 						<button className="btn btn-sm btn-outline-info"><span className="icon icon-download"></span> Buy All: Free</button>
 					</div>
 				</li>
+                {/*@TODO change to arrow functino*/}
 				{DisplayFiles.map(function(file, i){
-					return <li key={i} onClick={() => {_this.handleListClick(_this.props.Artifact, file) } } className="list-group-item" style={file.info.getFilename() === _this.props.ActiveFile.info.getFilename() ? {padding: "0px", backgroundColor: _this.props.mainColor, border: "1px solid " + _this.props.mainColor} : {padding: "0px", backgroundColor: _this.props.bgColor, border: "1px solid " + _this.props.mainColor}}>
+					return <li key={i} onClick={() => {_this.handleListClick(_this.props.artifact, file) } } className="list-group-item" style={file.info.getFilename() === _this.props.activeFile.info.getFilename() ? {padding: "0px", backgroundColor: _this.props.mainColor, border: "1px solid " + _this.props.mainColor} : {padding: "0px", backgroundColor: _this.props.bgColor, border: "1px solid " + _this.props.mainColor}}>
 						<div style={{padding: "4px 5px", display:"flex"}}>
 							<img className="rounded" src={""} width="40px" height="40px" alt="" />
 							<div style={{padding: "0px 10px", width:"235px"}}>
-								<div style={file.info.getFilename() === _this.props.ActiveFile.info.getFilename() ? {color: _this.props.bgColor, fontWeight:"700",fontSize:"14px", textOverflow: "ellipsis", overflow: "hidden", whiteSpace: "nowrap"} : {color: _this.props.mainColor, fontWeight:"700",fontSize:"14px", textOverflow: "ellipsis", overflow: "hidden", whiteSpace: "nowrap"}}>{Artist ? Artist : "Unknown"}</div>
-								<div style={file.info.getFilename() === _this.props.ActiveFile.info.getFilename() ? {color: _this.props.bgColor, fontSize:"12px", width: "100%", display: "flex"} : {color: _this.props.mainColor, fontSize:"12px", width: "100%", display: "flex"}}>
+								<div style={file.info.getFilename() === _this.props.activeFile.info.getFilename() ? {color: _this.props.bgColor, fontWeight:"700",fontSize:"14px", textOverflow: "ellipsis", overflow: "hidden", whiteSpace: "nowrap"} : {color: _this.props.mainColor, fontWeight:"700",fontSize:"14px", textOverflow: "ellipsis", overflow: "hidden", whiteSpace: "nowrap"}}>{artist ? artist : "Unknown"}</div>
+								<div style={file.info.getFilename() === _this.props.activeFile.info.getFilename() ? {color: _this.props.bgColor, fontSize:"12px", width: "100%", display: "flex"} : {color: _this.props.mainColor, fontSize:"12px", width: "100%", display: "flex"}}>
 									<div style={{width: "200px", textOverflow: "ellipsis", overflow: "hidden", whiteSpace: "nowrap"}}>{i + 1}: {file.info.getDisplayName() ? file.info.getDisplayName() : file.info.getFilename()}</div>
 									<div style={{width: "30px", textAlign: "right"}}>
 										{file.info.getDuration() > 0 ? <FormattedTime numSeconds={file.info.getDuration()} /> : ""}
@@ -102,8 +103,8 @@ class Playlist extends Component {
 							</div>
 							<PaymentButtons
 								activeFile={file}
-								artifact={_this.props.Artifact}
-								btnStyle={file.info.getFilename() === _this.props.ActiveFile.info.getFilename() ? {backgroundColor: _this.props.bgColor} : {}}
+								artifact={_this.props.artifact}
+								btnStyle={file.info.getFilename() === _this.props.activeFile.info.getFilename() ? {backgroundColor: _this.props.bgColor} : {}}
 								// For Payment Buttons
                                 payForFileFunc={_this.props.payForFileFunc}
                                 buyFileFunc={_this.props.buyFileFunc}
@@ -117,6 +118,20 @@ class Playlist extends Component {
 			</ul>
 		);
 	}
+}
+
+Playlist.propTypes = {
+    mainColor: PropTypes.string,
+    bgColor: PropTypes.string,
+    currentArtifactOnly: PropTypes.bool,
+    filter: PropTypes.object,
+    artifact: PropTypes.object,
+    activeFile: PropTypes.object,
+    filePlaylist: PropTypes.object,
+    setCurrentFile: PropTypes.func,
+    payForFileFunc: PropTypes.func,
+    buyFileFunc: PropTypes.func,
+    isPlayingFile: PropTypes.func
 }
 
 export default Playlist;
