c96c2387f83be400d1464cf62b5fa30d8da8dd77
Ryan <ryan.chacon@alexandria.io>
2018-06-21 12:35:52 -0700

    added prop-types and props => camelCase

--- src/components/HtmlContainer.js
+++ src/components/HtmlContainer.js
@@ -1,15 +1,12 @@
 import React, { Component } from 'react';
+import PropTypes from "prop-types";
 
 class HTMLContainer extends Component {
-	constructor(props){
-		super(props);
-	}
-
 	render() {
 		let websiteURL;
 
-		if (this.props.ActiveFile && this.props.Artifact && this.props.ActiveFile.info){
-			websiteURL = this.props.buildIPFSURL(this.props.Artifact.getLocation(), this.props.ActiveFile.info.getFilename());
+		if (this.props.activeFile && this.props.artifact && this.props.activeFile.info){
+			websiteURL = this.props.buildIPFSURL(this.props.artifact.getLocation(), this.props.activeFile.info.getFilename());
 		}
 		return (
 			<iframe title="html_doc" style={{width:"inherit", height:"100%", overflow: "hidden", backgroundColor: "#fff", borderBottom: "1px solid rgba(0,0,0,0.2)"}} frameBorder="0" src={websiteURL}></iframe>
@@ -18,5 +15,11 @@ class HTMLContainer extends Component {
 }
 
 HTMLContainer.SUPPORTED_FILE_TYPES = ["html"];
+HTMLContainer.propTypes = {
+    artifact: PropTypes.object.isRequired,
+    activeFile: PropTypes.object.isRequired,
+    buildIPFSShortURL: PropTypes.func.isRequired,
+    buildIPFSURL: PropTypes.func.isRequired,
+};
 
 export default HTMLContainer;
