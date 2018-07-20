9637be85b0b2f8c625b5b25da3d7ec278ad27392
Ryan <ryan.chacon@alexandria.io>
2018-07-20 11:06:03 -0700

    shorten html tag

--- src/components/Playlist.js
+++ src/components/Playlist.js
@@ -84,7 +84,7 @@ class Playlist extends Component {
 				<li className="list-group-item" style={{padding: "5px 30px", display:"flex", backgroundColor: this.props.bgColor, border: "1px solid " + this.props.mainColor}}>
 					<div style={{margin: "auto"}}>
 						<button className="btn btn-sm btn-outline-info"><span className="icon icon-controller-play"></span>Play All: Free</button>
-						<span style={{paddingLeft: "10px"}}></span>
+						<span style={{paddingLeft: "10px"}}/>
 						<button className="btn btn-sm btn-outline-info"><span className="icon icon-download"></span> Buy All: Free</button>
 					</div>
 				</li>
@@ -109,6 +109,7 @@ class Playlist extends Component {
                                 paymentError={_this.props.paymentError}
                                 paymentInProgress={_this.props.paymentInProgress}
                                 payForFile={_this.props.payForFile}
+                                isPlayingFile={_this.props.isPlayingFile}
                             />
                             <BuyFileButton
                                 artifact={_this.props.artifact}
