fb2a7e758bbb558fb1536c3ec26b866aefec84ce
Ryan <ryanachacon@gmail.com>
2018-07-30 11:08:38 -0700

    removed useless constructor

--- src/components/ModalCoin.js
+++ src/components/ModalCoin.js
@@ -2,21 +2,6 @@ import React from 'react'
 import {RANDOM_ARTIFACT_LIST} from "../actions/ArtifactLists/actions";
 
 class ModalCoin extends React.Component {
-    constructor(props) {
-        super(props)
-
-        this.state = {
-            coinBalance: undefined,
-            coinMainAddress: undefined
-        }
-    }
-    // componentDidMount() {
-    //     this.setState({
-    //         coinBalance: this.props.acc.wallet.getCoin(this.props.coinName).getBalance(),
-    //         // coinMainAddress: this.props.acc.wallet.getCoin(this.props.coinName).getAddress().getPublicAddress()
-    //     })
-    // }
-
     render() {
         return (
             <div className="col">
