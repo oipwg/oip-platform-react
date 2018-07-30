fa483410e092b848ccdb68fe4d41dbbb9d132b25
Ryan <ryanachacon@gmail.com>
2018-07-29 18:39:39 -0700

    filter NSFW

--- src/actions/ArtifactLists/thunks.js
+++ src/actions/ArtifactLists/thunks.js
@@ -13,34 +13,40 @@ export const fetchArtifactList = (list_id, options) => (dispatch, getState) => {
     dispatch(requestArtifactList(list_id));
     let state = getState();
 
+    let filteredArtifacts = [];
+    const filterArtifacts = (artifacts) => {
+        for (let art of artifacts) {
+            if (!art.getNSFW()) {filteredArtifacts.push(art)}
+        }
+    }
     if (list_id === LATEST_CONTENT_LIST){
         state.OIPIndex.Index.getLatestArtifacts(100)
             .then(arts => {
-                dispatch(recieveArtifactList(list_id, arts));
+                filterArtifacts(arts)
+                dispatch(recieveArtifactList(list_id, filteredArtifacts));
             })
             .catch(err => {
                 dispatch(requestArtifactListError(list_id, err));
-                console.log(err)
             })
     } else if (list_id === SEARCH_PAGE_LIST) {
         //@ToDo::ElasticSearch
     } else if (list_id === PUBLISHER_PAGE_LIST) {
         state.OIPIndex.Index.getArtifacts(undefined, undefined, undefined, undefined, options)
             .then(arts => {
-                dispatch(recieveArtifactList(list_id, arts));
+                filterArtifacts(arts)
+                dispatch(recieveArtifactList(list_id, filteredArtifacts));
             })
             .catch(err => {
                 dispatch(requestArtifactListError(list_id, err));
-                console.log(err)
             })
     } else {
         state.OIPIndex.Index.getLatestArtifacts(33)
             .then(arts => {
-                dispatch(recieveArtifactList(list_id, arts));
+                filterArtifacts(arts)
+                dispatch(recieveArtifactList(list_id, filteredArtifacts));
             })
             .catch(err => {
                 dispatch(requestArtifactListError(list_id, err));
-                console.log(err)
             })
     }
 };
