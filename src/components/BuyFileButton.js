4a426aa39d3ed3e1fb329f686e9bae4423e1df0f
orpheus <ryanachacon@gmail.com>
2018-08-13 13:31:47 -0700

    scrap payment logic

--- src/components/BuyFileButton.js
+++ src/components/BuyFileButton.js
@@ -1,123 +1,26 @@
 import React, { Component } from 'react';
 import PropTypes from "prop-types";
 import {connect} from 'react-redux'
-import RefillModal from './RefillModal'
-
-import {loginPrompt} from '../actions/User/actions'
-
 
 class BuyFileButton extends Component {
     constructor(props){
         super(props);
 
-        this.state = {
-            refillModal: false
-        }
-
         this.buyFile = this.buyFile.bind(this);
         this.createPriceString = this.createPriceString.bind(this);
-        this.checkLogin = this.checkLogin.bind(this)
-        this.pay = this.pay.bind(this)
-        this.attemptPayment = this.attemptPayment.bind(this)
-        this.toggleRefillModal = this.toggleRefillModal.bind(this)
     }
 
-    toggleRefillModal() {
-        this.setState({refillModal: !this.state.refillModal})
-    }
-
-    pay(ret) {
-
-        console.log("attempting to send payment: ", ret)
-        let coins = []
-        for (let coinTicker of Object.keys(ret)) {
-            coins.push(coinTicker)
-        }
-        let paymentAddress = this.state.paymentAddresses[coins[0]];
-        let paymentAmount = ret[coins[0]].cryptoFileCost
-
-        console.log(`Payment address: ${paymentAddress}, Payment Amount: ${paymentAmount}, Payment Coin: ${this.state.ap.tickerToName(coins[0])}`)
-
-        if (this.props.file.isPaid && !this.props.file.hasPaid) {
-            this.props.buyInProgress(this.props.file.key)
-            this.state.ap.sendPayment(this.state.paymentAddresses[coins[0]], 0.0001357, this.state.ap.tickerToName(coins[0]))
-                .then(data => {
-                    this.props.buyFile(this.props.file.key)
-                    console.log('Succesfully paid for artifact file: ', data)
-                })
-                .catch(err => {
-                    this.props.buyError(this.props.file.key)
-                    console.log("Error while trying to pay for artifact file: ", err)
-                })
-        }
-        this.props.setCurrentFile(this.props.artifact, this.props.file);
-    }
-
-
-    attemptPayment() {
-        return new Promise( (res, rej) => {
-            let acc = this.props.account;
-            let ap = acc.getPaymentBuilder(this.props.account.wallet, this.props.artifact, this.props.file.info, "buy")
-            console.log("Payment amount: ", ap.getPaymentAmount())
-            this.setState({
-                ap: ap,
-                addresses: this.props.wallet.addresses,
-                supportedCoins: ap.getSupportedCoins(),
-                paymentAddresses: ap.getPaymentAddresses()
-            });
-
-            console.log("Pay vars", ap.getSupportedCoins(), ap.getPaymentAmount(), this.props.wallet.cryptoBalances)
-            ap.getCoinsWithSufficientBalance(this.props.wallet.cryptoBalances ? this.props.wallet.cryptoBalances : {btc: 0, ltc: 0, flo: 0},
-                ap.getSupportedCoins(), ap.getPaymentAmount(), {all: true})
-                .then((ret) => {
-                    console.log("return val", ret)
-                    if (Array.isArray(ret)) {
-                        if (ret.length === 0 ) {
-                            rej({error: "Insufficient Balance", ret})
-                        }
-                    } else {
-                        if (Object.keys(ret).length > 0) {
-                            res(ret)
-                        } else {
-                            rej({error: "Insufficient Balance ", ret})
-                        }
-                    }
-                })
-                .catch(err => rej(err))
-        })
-    }
-    checkLogin() {
-        return new Promise( (res, rej) => {
-            (this.props.User.isLoggedIn ? res() : rej() )
-        })
-    }
     buyFile(){
         if (this.props.file.info && this.props.file.info.getSuggestedBuyCost() == 0) {
             console.log("buying file: ", this.props.file.key)
             this.props.buyFile(this.props.file.key)
-            //Do I need this?
+
             if (this.props.file.info.getType() === 'Audio') {
                 this.props.isPlayingFile(this.props.file.key, !this.props.file.isPlaying)
             }
             if (this.props.file.key !== this.props.activeFile.key) {this.props.setCurrentFile(this.props.artifact, this.props.file)}
             return
         }
-        this.checkLogin()
-            .then( () => {
-                this.attemptPayment()
-                    .then(ret => {
-                        this.pay(ret)
-                    })
-                    .catch(err => {
-                        if (err.error) {
-                            console.log("attempt payment threw: ", err)
-                            this.toggleRefillModal()
-                        } else {alert("Need to load wallet balances first! One sec...")}
-                    })
-            })
-            .catch( (e) => {
-                this.props.loginPrompt(true)
-            })
     }
 
     createPriceString(price){
--- src/components/ViewFileButton.js
+++ src/components/ViewFileButton.js
@@ -1,122 +1,25 @@
 import React, { Component } from 'react';
 import PropTypes from "prop-types";
 import {connect} from 'react-redux'
-import {loginPrompt} from '../actions/User/actions'
-import RefillModal from "./RefillModal";
 
 class ViewFileButton extends Component {
     constructor(props){
         super(props);
 
-        this.state = {
-            refillModal: false
-        }
-
         this.viewFile = this.viewFile.bind(this);
         this.createPriceString = this.createPriceString.bind(this);
-        this.checkLogin = this.checkLogin.bind(this)
-        this.pay = this.pay.bind(this)
-        this.attemptPayment = this.attemptPayment.bind(this)
-        this.toggleRefillModal = this.toggleRefillModal.bind(this)
-
     }
 
-    toggleRefillModal() {
-        this.setState({refillModal: !this.state.refillModal})
-    }
-
-    pay(ret) {
-        console.log("attempting to send payment: ", ret)
-        let coins = []
-        for (let coinTicker of Object.keys(ret)) {
-            coins.push(coinTicker)
-        }
-        let paymentAddress = this.state.paymentAddresses[coins[0]];
-        let paymentAmount = Number.parseFloat(ret[coins[0]].cryptoFileCost).toFixed(7);
-
-        console.log(`Payment address: ${paymentAddress}, Payment Amount: ${paymentAmount}, Payment Coin: ${this.state.ap.tickerToName(coins[0])}`)
-
-
-        if (this.props.file.isPaid && !this.props.file.hasPaid) {
-            this.props.paymentInProgress(this.props.file.key)
-            this.state.ap.sendPayment(this.state.paymentAddresses[coins[0]], 0.0001357, this.state.ap.tickerToName(coins[0]))
-                .then(data => {
-                    this.props.payForFile(this.props.file.key)
-                   if( data ) {
-                       console.log("Payment successful: ", data)
-                   }
-                })
-                .catch(err => {
-                    this.props.paymentError(this.props.file.key)
-                    console.log("Error while trying to pay for artifact file: ", err)
-                })
-        }
-        this.props.setCurrentFile(this.props.artifact, this.props.file);
-    }
-    attemptPayment() {
-        return new Promise( (res, rej) => {
-            let acc = this.props.account;
-            let ap = acc.getPaymentBuilder(this.props.account.wallet, this.props.artifact, this.props.file.info, "view")
-            console.log("Payment amount: ", ap.getPaymentAmount())
-            this.setState({
-                ap: ap,
-                addresses: this.props.wallet.addresses,
-                supportedCoins: ap.getSupportedCoins(),
-                paymentAddresses: ap.getPaymentAddresses()
-            });
-
-            console.log("Pay vars", ap.getSupportedCoins(), ap.getPaymentAmount(), this.props.wallet.cryptoBalances)
-            ap.getCoinsWithSufficientBalance(this.props.wallet.cryptoBalances ? this.props.wallet.cryptoBalances : {btc: 0, ltc: 0, flo: 0},
-                ap.getSupportedCoins(), ap.getPaymentAmount(), {all: true})
-                .then(ret => {
-                    console.log("return val", ret)
-                    if (Array.isArray(ret)) {
-                        if (ret.length === 0 ) {
-                            rej({error: "Insufficient Balance", ret})
-                        }
-                    } else {
-                        if (Object.keys(ret).length > 0) {
-                            res(ret)
-                        } else {
-                            rej({error: "Insufficient Balance ", ret})
-                        }
-                    }
-                })
-                .catch(err => rej(err))
-        })
-    }
-    checkLogin() {
-        return new Promise( (res, rej) => {
-            (this.props.User.isLoggedIn ? res() : rej() )
-        })
-    }
     viewFile(){
         if (this.props.file.info && this.props.file.info.getSuggestedPlayCost() == 0) {
             this.props.payForFile(this.props.file.key)
-            //Do I need this?
+
             if (this.props.file.info.getType() === 'Audio') {
                 this.props.isPlayingFile(this.props.file.key, !this.props.file.isPlaying)
             }
             if (this.props.file.key !== this.props.activeFile.key) {this.props.setCurrentFile(this.props.artifact, this.props.file)}
             return
         }
-        this.checkLogin()
-            .then( () => {
-                this.attemptPayment()
-                    .then(ret => {
-                        this.pay(ret)
-                    })
-                    .catch(err => {
-                        if (err.error) {
-                            console.log("attempt payment threw: ", err)
-                            this.toggleRefillModal()
-                        } else {alert("Still need to load wallet balances!")}
-                    })
-            })
-            .catch( (e) => {
-                this.props.loginPrompt(true)
-            })
-
     }
 
     createPriceString(price){
@@ -205,10 +108,6 @@ class ViewFileButton extends Component {
         }
         return (
             <div style={{display: disallowPlay ? "" : "inline-block", paddingRight: "3px"}}>
-                {this.state.refillModal ? <RefillModal addresses={this.state.addresses} supportedCoins={this.state.supportedCoins}
-                                                       account={this.props.account} wallet={this.props.wallet} ap={this.state.ap}
-                                                       cryptoBalances={this.props.wallet.cryptoBalances}
-                                                       isOpen={this.state.refillModal} toggleModal={this.toggleRefillModal}/> : ""}
                 { disallowPlay ? "" :
                     <button  className={"pad-5 btn btn-" + viewBtnType} onClick={this.viewFile} style={this.props.btnStyle} >
                         <span className="icon icon-controller-play" style={{marginRight: "5px"}}/>{viewString}
@@ -237,7 +136,4 @@ function mapStateToProps(state) {
     }
 }
 
-const mapDispatchToProps = {
-    loginPrompt
-}
-export default connect(mapStateToProps, mapDispatchToProps)(ViewFileButton)
+export default connect(mapStateToProps)(ViewFileButton)
